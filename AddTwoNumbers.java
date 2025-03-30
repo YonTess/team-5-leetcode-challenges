7781ffdfb2396437b90086b5b5ef3d2170155a2c
Yonatan G <78848157+YonTess@users.noreply.github.com>
2025-03-30 19:59:34 -0400

    Add comments to challeges 4 and 5

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,34 +1,46 @@
+import java.lang.classfile.components.ClassPrinter.ListNode;
+
 public class AddTwoNumbers {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         
+        // Create a dummy node to store the linked list result 
         ListNode result = new ListNode(0);
+
+        // Set pointer to the empty result node
         ListNode pt = result;
 
+        // initialize carry to store carried values
         int carry = 0;
 
+        // Loop through both linked lists
         while (l1 != null || l2 != null) {
             
             int sum = 0 + carry;
 
+            // If l1 has value, add it to the sum and jump to the next node
             if (l1 != null) {
                 sum += l1.val;
                 l1 = l1.next;
             }
 
+            // If l2 has value, add it to the sum and jump to the next node
             if (l2 != null) {
                 sum += l2.val;
                 l2 = l2.next;
             }
             
-            carry = sum /10;
-            sum = sum % 10;
-            pt.next = new ListNode(sum);
-            pt = pt.next;
+            carry = sum /10;    // Calculate the new carry
+            sum = sum % 10;     // Keep only the modulus    
+            pt.next = new ListNode(sum);      // create a new node with sum and link to the result list
+            pt = pt.next;       // move pointer to the next node
         }
+
+        // Add remaining carry to the new node
         if (carry == 1) {
             pt.next = new ListNode(1);
         }
 
+        // Return the head of the result
         return result.next;
     }
 }
--- MergeTwoLists.java
+++ MergeTwoLists.java
@@ -1,11 +1,17 @@
 public class MergeTwoLIsts {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
+        // Create a dummy node to store the linked list result
         ListNode result = new ListNode(0);
         
+        // Set pointer to track the current position in the new list
         ListNode current = result;
 
+        // Loop through both linked lists as long as neither are null
         while (list1 != null && list2 != null) {
+
+            // compare the current nodes of the two lists 
+            // and attach the smaller one
 
             if (list1.val <= list2.val) {
                 current.next = list1;
@@ -14,8 +20,11 @@ public class MergeTwoLIsts {
                 current.next = list2;
                 list2 = list2.next;
         }
+
+        // Move the current pointer to the next value
         current = current.next; 
-            
+          
+        // Attach any remaining nodes from either lists
         }
         if (list1 != null) {
             current.next = list1;
@@ -23,6 +32,7 @@ public class MergeTwoLIsts {
             current.next = list2;
         }
         
+        // Return the head of the result
         return result.next;
     }
    
